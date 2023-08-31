7a7b5944e67bfe8b081152afbe574f669d823e43
malayanand <anandmalay6@gmail.com>
2023-08-31 23:10:30 +0530

    changed typewriter message

--- components/Typewriter-effect.js
+++ components/Typewriter-effect.js
@@ -6,26 +6,20 @@ const Typewriter = (props) => {
     <Box
       className="typewriter"
       textAlign={"left"}
-      color={useColorModeValue("#F47C7C", "#4BE3AC")}
+      color={"#4F4D4D"}
       fontSize={{ base: "0.9rem", md: "1rem" }}
     >
       <TypeIt
         // speed="10"
         // autoStart = "true"
         options={{
-          loop: true,
+          loop:false,
           cursor: "#F47C7C",
         }}
         getBeforeInit={(instance) => {
           instance
-            .type("Django")
+            .type("Currently a software developer @ Daimler Truck Innovation Center India")
             .pause(750)
-            .delete(6)
-            .pause(500)
-            .type("Python, C/C++")
-            .pause(750)
-            .delete(13)
-            .type("React JS, Next JS");
 
           // Remember to return it!
           return instance;
